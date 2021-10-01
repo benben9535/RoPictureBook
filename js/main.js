1f81f05736fa94237cfb783298aa4831f920b7da
benben <benben9535@gmail.com>
2021-10-01 15:56:04 +0800

    fix: 修正導覽按bug
    - 修正使用上/下一頁按鈕換頁時，導覽列按鈕樣式未變更的問題
    - 增加meta 關鍵字、敘述
    - 修改標題

--- js/main.js
+++ js/main.js
@@ -9,24 +9,25 @@ function initBook() {
   book.bookblock({
     speed : 800,
 		shadowSides : 0.8,
-		shadowFlip : 0.7
+		shadowFlip : 0.7,
+    onEndFlip : function(old, page, isLimit) { 
+      $('.btn-wrap').children().removeClass('current');
+      $(`.btn-wrap :eq(${page})`).addClass('current');
+    }
   });
 
   // add navigation events
   $('.button-next').on('click touchstart', function() {
     book.bookblock('next');
-    return false;
   });
 
   $('.button-prev').on('click touchstart', function() {
     book.bookblock('prev');
-    return false;
   });
 
   $('.catalog-item').each( function(i) {
     $(this).on('click touchstart', function() {
       book.bookblock('jump', i + 2);
-      return false;
     });
   });
 
@@ -35,22 +36,16 @@ function initBook() {
       book.bookblock('jump', i + 1);
       $('.btn-wrap').children().removeClass('current');
       $(this).addClass('current');
-      return false;
     });
   });
 
-
-  
-  
   // add swipe events
   slides.on({
     'swipeleft' : function() {
       book.bookblock('next');
-      return false;
     },
     'swiperight' : function() {
       book.bookblock('prev');
-      return false;
     }
   });
 }
